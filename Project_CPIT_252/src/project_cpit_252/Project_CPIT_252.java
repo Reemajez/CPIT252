9d95874e4487be4584ef9dbc78429d9f4ba4999d
Reema <reemajezz@hotmail.com>
2024-11-01 15:21:29 +0300

    this push for Reema!

---
+++ Project_CPIT_252/src/project_cpit_252/Project_CPIT_252.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project_cpit_252;
+
+/**
+ *
+ * @author reemajez
+ */
+public class Project_CPIT_252 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hi this is reema ");
+    }
+    
+}
