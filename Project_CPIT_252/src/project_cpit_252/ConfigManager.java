ec37b1295c93fa9b3449e0f0c80f2f7921382772
Reema <reemajezz@hotmail.com>
2024-11-01 15:25:53 +0300

    this push for singelton

---
+++ Project_CPIT_252/src/project_cpit_252/ConfigManager.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project_cpit_252;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author reemajez
+ */
+public class ConfigManager {
+    private static ConfigManager instance;
+    private Map<String, String> config;
+
+    private ConfigManager() {
+        config = new HashMap<>();
+        config.put("currency", "USD");
+        config.put("region", "US");
+    }
+
+    public static ConfigManager getInstance() {
+        if (instance == null) {
+            instance = new ConfigManager();
+        }
+        return instance;
+    }
+
+    public String getConfig(String key) {
+        return config.get(key);
+    }
+}
+
